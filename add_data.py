86abafcaaafde2fb023a04e093b5f6e230121f48
Renan-Bodom <renan1211@hotmail.com>
2025-04-14 08:23:58 -0300

    Tratando loops em gerações

--- add_data.py
+++ add_data.py
@@ -79,7 +79,8 @@ df_optimized["year"] = pd.to_numeric(df_optimized["year"], errors="coerce")     
 df_optimized["num_advisors"] = df_optimized["advisor_ids"].apply(len)                                 # Contar quantos orientadores cada título teve e adiciona a coluna
 
 # Calcular o número de alunos por pesquisador
-tqdm.pandas(desc="Calculando número de alunos")
+print("Iniciando processamento dos dados...\n")
+tqdm.pandas(desc="Calculando número de alunos de um pesquisador")
 df_optimized["num_students"] = df_optimized["researcher_id"].progress_apply(lambda rid: len(get_advisor_and_student_ids(rid)[1]))
 
 ## Salvando os novos dados
@@ -103,6 +104,7 @@ def count_siblings(idx, advisors):
     siblings.discard(idx)  # Remove ele mesmo
     return len(siblings)
 
+print("\n")
 tqdm.pandas(desc="Calculando número de irmãos")
 df_optimized["num_siblings"] = df_optimized.progress_apply(lambda row: count_siblings(row.name, row["advisor_ids"]), axis=1)
 
@@ -119,11 +121,25 @@ ids_validos = set(df_optimized["researcher_id"])
 # Cache para memoização (melhora performance e evita recálculo)
 geracao_cache = {}
 
-def cont_geracao(researcher_id):
+ciclos_detectados = []
+def cont_geracao(researcher_id, visited=None):
+    # Verifica se pesquisador já foi visitado, tratando loop
+    if visited is None:
+        visited = set()
+
+    
     # Se já calculamos a geração desse pesquisador, retornamos do cache
     if researcher_id in geracao_cache:
         #print("USOU CACHE")
         return geracao_cache[researcher_id]
+    
+    if researcher_id in visited:
+        print(f"Ciclo detectado: {researcher_id}")
+        ciclos_detectados.append(researcher_id)
+        geracao_cache[researcher_id] = -1  # ou algum valor default
+        return -1
+    
+    visited.add(researcher_id)
     
     orientadores = [int(oid) for oid in advisor_map.get(researcher_id, [])]
     
@@ -136,7 +152,7 @@ def cont_geracao(researcher_id):
     max_geracao = 0
     for oid in orientadores:
         if oid in ids_validos:
-            geracao_orientador = cont_geracao(oid)
+            geracao_orientador = cont_geracao(oid, visited.copy())
             max_geracao = max(max_geracao, geracao_orientador)
 
     geracao_final = max_geracao + 1
@@ -145,8 +161,10 @@ def cont_geracao(researcher_id):
     return geracao_final
 
 # Aplica a função em todos os pesquisadores
+print("\n")
 tqdm.pandas(desc="Calculando o número das gerações")
 df_optimized["generation"] = df_optimized["researcher_id"].progress_apply(cont_geracao)
+print("Total de ciclos detectados nas gerações:", len(ciclos_detectados), "\n")
 
 
 ## Salvando os novos dados
@@ -156,4 +174,4 @@ df_optimized.to_feather("./data/mgp_optimized.feather")
 ## Lendo o novo dataframe
 df_optimized = pd.read_feather("./data/mgp_optimized.feather")
 
-print("Novo banco:\n", df_optimized.tail(5), "\n\n...tarefa finalizada.")
+print("Novo banco:\n", df_optimized.tail(5), "\n\n...tarefa finalizada.\n")
